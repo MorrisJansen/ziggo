97b7d3a0a7afd677c9b3b0c290c9ba5984fb49a2
MorrisJansen <morris8jansen@hotmail.com>
2024-08-27 16:10:04 +0200

    gh-pages implementeren

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  publicPath : './'
-}
+  publicPath: process.env.NODE_ENV === 'production' ? '/ziggo/' : '/',
+  configureWebpack: {
+    optimization: {
+      minimize: false
+    }
+  }};
