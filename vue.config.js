1505b507064683422887b60bc9a64c797c885c46
MorrisJansen <morris8jansen@hotmail.com>
2024-08-27 16:11:48 +0200

    gh-pages fiksen nu zie ik alleen readme (nogsteeds)

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/ziggo/' : '/',
-  configureWebpack: {
-    optimization: {
-      minimize: false
-    }
-  }};
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/ziggo/'  // De naam van je repository is 'ziggo'
+    : '/'
+}
